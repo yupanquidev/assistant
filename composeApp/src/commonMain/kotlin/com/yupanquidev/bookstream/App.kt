2abf93118a98dcd39f4040276a67a13ab0635af1
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-09-16 11:32:51 -0500

    fix project

--- composeApp/src/commonMain/kotlin/com/yupanquidev/bookstream/App.kt
+++ composeApp/src/commonMain/kotlin/com/yupanquidev/bookstream/App.kt
@@ -19,7 +19,19 @@ import bookstream.composeapp.generated.resources.compose_multiplatform
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-       Text("Hello World")
+  MaterialTheme {
+    var showContent by remember { mutableStateOf(false) }
+    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+      Button(onClick = { showContent = !showContent }) {
+        Text("Click me!")
+      }
+      AnimatedVisibility(showContent) {
+        val greeting = remember { Greeting().greet() }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+          Image(painterResource(Res.drawable.compose_multiplatform), null)
+          Text("Compose: $greeting")
+        }
+      }
     }
+  }
 }
---
+++ composeApp/src/commonMain/kotlin/com/yupanquidev/bookstream/Platform.kt
@@ -0,0 +1,7 @@
+package com.yupanquidev.bookstream
+
+interface Platform {
+  val name: String
+}
+
+expect fun getPlatform(): Platform
